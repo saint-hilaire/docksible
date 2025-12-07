832903f4c6dffa6e36c314f2a1cb67141c745aa1
Belal Ibrahim <belal.ibrahim@proton.me>
2025-12-07 22:30:14 +0100

    v1.0.0-rc-1

--- src/docksible/cli.py
+++ src/docksible/cli.py
@@ -6,7 +6,7 @@ from .docksible import Docksible
 __author__ = "Belal Ibrahim"
 __copyright__ = "Copyright 2025 Belal Ibrahim"
 __license__ = "Apache License, Version 2.0"
-__version__ = "1.0.0-beta-3"
+__version__ = "1.0.0-rc-1"
 __maintainer__ = "Belal Ibrahim"
 __email__ = "belal.ibrahim@proton.me"
 
