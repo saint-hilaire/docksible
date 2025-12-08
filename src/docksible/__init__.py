4f05a1d48178541b2f68d812de9bbb071d25bddf
Belal Ibrahim <belal.ibrahim@proton.me>
2025-12-08 21:40:22 +0100

    release/1.0.0
    - Version bump: v1.0.0
    - Add __init__.py, CLI banner, minor improvements in arg validation.

---
+++ src/docksible/__init__.py
@@ -0,0 +1,6 @@
+__author__ = "Belal Ibrahim"
+__copyright__ = "Copyright 2025 Belal Ibrahim"
+__license__ = "Apache License, Version 2.0"
+__version__ = "1.0.0"
+__maintainer__ = "Belal Ibrahim"
+__email__ = "belal.ibrahim@proton.me"
