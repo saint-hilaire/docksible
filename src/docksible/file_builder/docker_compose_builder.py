7fda9a602ae3a8efbb5f35f6057063ce9b9b14b0
Belal Ibrahim <belal.ibrahim@proton.me>
2026-01-05 20:58:56 +0100

    maintenance
    - No more default authentication plugin 'mysql_native_password'
      during WordPress installation.

--- src/docksible/file_builder/docker_compose_builder.py
+++ src/docksible/file_builder/docker_compose_builder.py
@@ -45,11 +45,6 @@ class DockerComposeBuilder(DocksibleFileBuilder):
             self.docker_compose_services['docksible_db'] = \
                     yaml.safe_load(fh)['docksible_db']
 
-        if self.action == 'wordpress':
-            # TODO: Necessary? I saw this in the legacy version.
-            self.docker_compose_services['docksible_db']['command'] = \
-                    '--default-authentication-plugin=mysql_native_password'
-
         if self.phpmyadmin:
             self.docker_compose_services['docksible_phpmyadmin'] = \
                     self.get_additional_template('phpmyadmin-service.yml.j2')[
